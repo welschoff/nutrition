bfc802d3c9d89161d4caa3aca0844a88866952f4
welschoff <h.welschoff@gmx.de>
2021-10-07 11:16:43 +0200

    avatar with icon and crown

---
+++ src/components/shambavi/women.stories.js
@@ -0,0 +1,9 @@
+import "./women.css";
+import women from "./women.html";
+
+export default {
+  title: "Components/women",
+  parameters: { layout: "centered" },
+};
+
+export const Image = () => women;
